d6f5d7f75c06bf3e0e0aa8f887bb296a388518b7
imveeru <vind.veeru@gmail.com>
2021-05-20 19:34:36 +0530

    Icon Update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 import React, { useEffect,useState } from 'react';
 import './App.css';
 
+document.title="Cryto-Track"
+
 //https: https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
 
 function App() {
@@ -15,14 +17,21 @@ function App() {
       console.log(res.data)
     })
     .catch(err=>{
-      console.log(err)
+      alert('Oops! Something went wrong.')
     })
 
   },[])
 
   return (
-    <div className="App">
-      <h1>API</h1>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Search  a Currency</h1>
+        <form>
+          <input type="text" className="coin-input    " placeholder="Search"></input>
+        </form>
+      </div>
+
+      
     </div>
   );
 }
