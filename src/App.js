069a6132c07e13197d4c57c8bb16d999bd52b000
imveeru <vind.veeru@gmail.com>
2021-05-20 19:23:39 +0530

    Fetched data using axios.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect,useState } from 'react';
 import './App.css';
 
 //https: https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
 
 function App() {
+
+  const [coin,setCoins]=useState([])
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res=>{
+      setCoins(res.data)
+      console.log(res.data)
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+
+  },[])
+
   return (
     <div className="App">
       <h1>API</h1>
